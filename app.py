140203d2ff322c8ea042a7f7b075eed72969dc7e
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-07-30 01:52:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -66,13 +66,10 @@ if st.session_state.messages[-1]["role"] != "assistant":
             response = generate_llama2_response(prompt)
             placeholder = st.empty()
             full_response = ''
-            # for item in response:
-            #     full_response += item
-            full_response += response.delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-            #    placeholder.markdown(full_response)
-            #placeholder.markdown(full_response)
+            for item in response:
+                full_response += item
+               placeholder.markdown(full_response)
+            placeholder.markdown(full_response)
             
         message = {"role": "assistant", "content": full_response}
         st.session_state.messages.append(message)
