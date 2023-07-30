47b49fb2bf38a24ae5969f148d7a02b3de84d983
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-07-30 01:07:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ with st.sidebar:
     st.markdown("1. Go to https://replicate.com/signin/")
     st.markdown("2. Sign in with your GitHub account.")
     st.markdown("3. Proceed to the API tokens page and copy your API token.")
+    st.markdown("Built by Josiah Adesola (from Streamlit docs)[https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/#1-get-a-replicate-api-token]")
     
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
